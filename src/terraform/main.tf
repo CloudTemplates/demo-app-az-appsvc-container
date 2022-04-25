0b47aae4d90754bbb7d74be74df40287aceb45c4
Preetish Madalia <preetish.madalia@aa.com>
2022-04-24 20:17:49 -0500

    removed unwanted variables

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -5,11 +5,7 @@ provider azurerm {
 module "appservice-demo" {
   source                   = "git::https://github.com/CloudTemplates/terraform-modules.git//azure-modules/appservice-linux?ref=main"
   appservice_plan_name     = var.appservice_plan_name
-  appservice_plan_tier     = ""
-  appservice_plan_size     = ""
-  appservice_plan_kind     = ""
   resource_group_name      = var.resource_group_name
-  appservice_plan_reserved = ""
   appservice               = var.appservice
   appservice_slot          = var.appservice_slot
   /*dynamic_app_settings     = {
